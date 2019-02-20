692e64b055d6f71f312f77573d727e6e9157dcb2
liuyajun <liuyajun@linhuiba.com>
2019-02-20 12:04:33 +0800

    修改

--- simple/webpack.config.js
+++ simple/webpack.config.js
@@ -39,54 +39,54 @@ module.exports = {
   */
   module: {
     rules: [
-    {
+      {
       /*
       使用 babel 编译 ES6 / ES7 / ES8 为 ES5 代码
       使用正则表达式匹配后缀名为 .js 的文件
       */
-      test: /\.js$/,
+        test: /\.js$/,
 
-      // 排除 node_modules 目录下的文件，npm 安装的包不需要编译
-      exclude: /node_modules/,
+        // 排除 node_modules 目录下的文件，npm 安装的包不需要编译
+        exclude: /node_modules/,
 
-      /*
+        /*
       use 指定该文件的 loader, 值可以是字符串或者数组。
       这里先使用 eslint-loader 处理，返回的结果交给 babel-loader 处理。loader 的处理顺序是从最后一个到第一个。
       eslint-loader 用来检查代码，如果有错误，编译的时候会报错。
       babel-loader 用来编译 js 文件。
       */
-      use: ['babel-loader', 'eslint-loader']
-    },
-    {
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
       // 匹配 html 文件
-      test: /\.html$/,
-      /*
+        test: /\.html$/,
+        /*
       使用 html-loader, 将 html 内容存为 js 字符串，比如当遇到
       import htmlString from './template.html';
       template.html 的文件内容会被转成一个 js 字符串，合并到 js 文件里。
       */
-      use: 'html-loader'
-    },
-    {
+        use: 'html-loader'
+      },
+      {
       // 匹配 css 文件
-      test: /\.css$/,
+        test: /\.css$/,
 
-      /*
+        /*
       先使用 css-loader 处理，返回的结果交给 style-loader 处理。
       css-loader 将 css 内容存为 js 字符串，并且会把 background, @font-face 等引用的图片，
       字体文件交给指定的 loader 打包，类似上面的 html-loader, 用什么 loader 同样在 loaders 对象中定义，等会下面就会看到。
       */
-      use: ['style-loader', 'css-loader']
-    },
-    {
+        use: ['style-loader', 'css-loader']
+      },
+      {
       /*
       匹配各种格式的图片和字体文件
       上面 html-loader 会把 html 中 <img> 标签的图片解析出来，文件名匹配到这里的 test 的正则表达式，
       css-loader 引用的图片和字体同样会匹配到这里的 test 条件
       */
-      test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
 
-      /*
+        /*
       使用 url-loader, 它接受一个 limit 参数，单位为字节(byte)
 
       当文件体积小于 limit 时，url-loader 把文件转为 Data URI 的格式内联到引用的地方
@@ -102,14 +102,14 @@ module.exports = {
       会被编译成
       <img src="/f78661bef717cf2cc2c2e5158f196384.png">
       */
-      use: [
-      {
-        loader: 'url-loader',
-        options: {
-          limit: 10000
-        }
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }]
       }]
-    }]
   },
   /*
   配置 webpack 插件
@@ -137,7 +137,7 @@ module.exports = {
       因为和 webpack 4 的兼容性问题，chunksSortMode 参数需要设置为 none
       https://github.com/jantimon/html-webpack-plugin/issues/870
       */
-      chunksSortMode: "none"
+      chunksSortMode: 'none'
     })
   ]
 }
